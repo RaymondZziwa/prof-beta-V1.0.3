fd91de97cfb5d31357af8f42e4e13b1654f0689f
RaymondZziwa <raymondzian@gmail.com>
2024-01-10 05:59:54 +0300

    working on the settings form functionality

--- src/components/settings/settings.js
+++ src/components/settings/settings.js
@@ -1,12 +1,42 @@
 import { Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import '../Namungoona/supervisor dashboard/namungoona.css'
 import Navbar from "../side navbar/sidenav";
+import { useState } from "react";
+import axios from "axios";
+import { Password } from "@mui/icons-material";
 
 const AccountSettings = () => {
+    const [oldUserName, setOldUserName] = useState('')
+    const [newUserName, setNewUserName] = useState('')
+    const [confirmNewUserName, setConfirmUserName] = useState('')
 
-    const resetUsername = event => {}
-    const resetPassword = event => {}
+    const [oldPwd, setOldPwd] = useState('')
+    const [newPwd, setNewPwd] = useState('')
+    const [confirmNewPwd, setConfirmNewPwd] = useState('')
+
+    const [passwordMatch, setPasswordMatch] = useState(false)
+
+    const validatedPassword = () => {
+        if (newPwd === confirmNewPwd) setPasswordMatch(true)
+    }
+    
+    const resetUsername = async event => {
+        event.preventDefault()
+        let res = await axios.post('http://82.180.136.230:3005/resetusername', {
+            token: localStorage.getItem('token'),
+            oldUserName: oldUserName,
+            newUserName: newUserName
+        })
+    }
+    const resetPassword = async event => {
+        event.preventDefault()
+        let res = await axios.post('http://82.180.136.230:3005/resetpassword', {
+            token: localStorage.getItem('token'),
+            username: oldUserName,
+            oldPassword: oldPwd,
+            password: confirmNewPwd
+        })
+    }
     
     return(
         <div className='container-fluid'>
@@ -30,7 +60,7 @@ const AccountSettings = () => {
                                     <input type="text" class="form-control" id="floatingPassword" placeholder="Password" required/>
                                     <label for="floatingPassword">Confirm New Username</label>
                                 </div><br></br>
-                                <button className="btn btn-outline-primary" style={{marginTop:'10px'}}>Reset Username</button>
+                                <button className="btn btn-outline-primary" style={{marginTop:'10px'}} onClick={resetUsername}>Reset Username</button>
                             </form>
 
                             <form style={{marginTop:'10px'}}> 
@@ -47,7 +77,7 @@ const AccountSettings = () => {
                                     <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required/>
                                     <label for="floatingPassword">Confirm New Password</label>
                                 </div><br></br>
-                                <button className="btn btn-outline-primary" style={{marginTop:'10px'}}>Reset Password</button>
+                                <button className="btn btn-outline-primary" style={{marginTop:'10px'}} onClick={resetPassword}>Reset Password</button>
                             </form>
 
                             
